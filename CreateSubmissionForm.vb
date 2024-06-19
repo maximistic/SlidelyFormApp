8b92f4165a1e7fc69946d442b7a0db5a8a573e21
maximistic <110153672+maximistic@users.noreply.github.com>
2024-06-19 10:45:03 +0530

    com1

---
+++ CreateSubmissionForm.vb
@@ -0,0 +1,46 @@
+﻿Public Class CreateSubmissionForm
+    Private stopwatchRunning As Boolean = False
+    Private stopwatchElapsed As TimeSpan = TimeSpan.Zero
+
+    Private Sub btnToggleStopwatch_Click(sender As Object, e As EventArgs) Handles btnToggleStopwatch.Click
+        stopwatchRunning = Not stopwatchRunning
+
+        If stopwatchRunning Then
+            Timer1.Start()
+            btnToggleStopwatch.Text = "PAUSE STOPWATCH"
+        Else
+            Timer1.Stop()
+            btnToggleStopwatch.Text = "RESUME STOPWATCH"
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        stopwatchElapsed = stopwatchElapsed.Add(TimeSpan.FromSeconds(1))
+        lblStopwatch.Text = stopwatchElapsed.ToString("hh\:mm\:ss")
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        ' Handle submission to backend here
+        Dim name As String = txtName.Text
+        Dim email As String = txtEmail.Text
+        Dim phone As String = txtPhone.Text
+        Dim githubRepo As String = txtGitHub.Text
+        ' Send this data to backend or save to a database, etc.
+
+        ' Optionally, reset form fields or perform other actions after submission
+        ResetForm()
+    End Sub
+
+    Private Sub ResetForm()
+        ' Reset form fields and stopwatch display
+        txtName.Text = ""
+        txtEmail.Text = ""
+        txtPhone.Text = ""
+        txtGitHub.Text = ""
+        lblStopwatch.Text = "00:00:00"
+        stopwatchElapsed = TimeSpan.Zero
+        stopwatchRunning = False
+        btnToggleStopwatch.Text = "TOGGLE STOPWATCH"
+        Timer1.Stop()
+    End Sub
+End Class
---
+++ ViewSubmissionsForm.vb
@@ -0,0 +1,50 @@
+﻿Public Class ViewSubmissionsForm
+    Private currentSubmissionIndex As Integer = 0
+    Private submissions As List(Of Submission)
+
+    Private WithEvents btnNext As Button
+    Private WithEvents btnPrevious As Button
+    Private lblName As Label
+    Private lblEmail As Label
+    Private lblPhone As Label
+    Private lblGitHub As Label
+
+    ' Constructor to initialize the form
+    Public Sub New()
+        InitializeComponent()
+        ' Initialize other components or data as needed
+    End Sub
+
+    Private Sub ViewSubmissionsForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Initialize submissions data (you need to populate this list with actual data)
+        submissions = New List(Of Submission)()
+        ' Populate submissions list with data from backend or wherever it's stored
+
+        ' Show the first submission by default
+        ShowSubmission(currentSubmissionIndex)
+    End Sub
+
+    Private Sub ShowSubmission(index As Integer)
+        If index >= 0 AndAlso index < submissions.Count Then
+            Dim submission As Submission = submissions(index)
+            lblName.Text = submission.Name
+            lblEmail.Text = submission.Email
+            lblPhone.Text = submission.Phone
+            lblGitHub.Text = submission.GitHubRepo
+        End If
+    End Sub
+
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        If currentSubmissionIndex < submissions.Count - 1 Then
+            currentSubmissionIndex += 1
+            ShowSubmission(currentSubmissionIndex)
+        End If
+    End Sub
+
+    Private Sub btnPrevious_Click(sender As Object, e As EventArgs) Handles btnPrevious.Click
+        If currentSubmissionIndex > 0 Then
+            currentSubmissionIndex -= 1
+            ShowSubmission(currentSubmissionIndex)
+        End If
+    End Sub
+End Class
